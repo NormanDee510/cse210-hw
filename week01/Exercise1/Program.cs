d9ecd5b2fa4b5c47ca99ce79a2e3773305440df5
NormanDee510 <111991706+NormanDee510@users.noreply.github.com>
2025-01-11 16:41:32 +0200

    my exercises for week one

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-         Console.Write("What is your first name? ");
+        Console.Write("What is your first name? ");
         string name = Console.ReadLine();
 
         Console.Write("What is your last name? ");
