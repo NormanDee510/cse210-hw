57a605d21f6c4ed10403a3a18cb28efedb2fcf67
NormanDee510 <111991706+NormanDee510@users.noreply.github.com>
2025-01-15 19:46:34 +0200

    week 2 exercise

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job
+        {
+            _jobTitle = "Software Engineer",
+            _company = "Cortexware",
+            _startYear = 2019,
+            _endYear = 2022
+        };
+
+        Job job2 = new Job
+        {
+            _jobTitle = "Manager",
+            _company = "Andiccio24",
+            _startYear = 2022,
+            _endYear = 2023
+        };
+
+        Resume myResume = new Resume
+        {
+            _name = "Newman Demba"
+        };
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
