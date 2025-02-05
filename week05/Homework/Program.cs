d1874d576373f8ddd9020d0e7f37d0971d3f1b7f
NormanDee510 <111991706+NormanDee510@users.noreply.github.com>
2025-02-05 22:10:15 +0200

    home work

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -2,8 +2,22 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test Assignment (Base Class)
+        Assignment assignment = new Assignment("Tatenda Chari", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        // Test MathAssignment
+        MathAssignment mathAssignment = new MathAssignment("Charles Carlos", "Fractions", "5.2", "7-15");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+        // Test WritingAssignment
+        WritingAssignment writingAssignment = new WritingAssignment("Mildred Dube", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
